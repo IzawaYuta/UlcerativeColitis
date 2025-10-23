4b95a99160e9ba80c219a259d0b4d4da12b8107d
yutaizawa <yutamac2512@icloud.com>
2025-10-23 19:57:09 +0900

    RealmSwift追加、モデル追加

---
+++ UlcerativeColitis/UlcerativeColitis/Model/Models.swift
@@ -0,0 +1,29 @@
+//
+//  Models.swift
+//  UlcerativeColitis
+//
+//  Created by Engineer MacBook Air on 2025/10/23.
+//
+
+import Foundation
+import RealmSwift
+
+enum StoolType: String, PersistableEnum {
+    case normal //普通便
+    case soft //軟便
+    case diarrhea //下痢
+    case blood //血便
+}
+
+class DayRecord: Object, Identifiable {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var date: Date
+    @Persisted var stoolRecord = List<StoolRecord>()
+}
+
+class StoolRecord: Object, Identifiable {
+    @Persisted(primaryKey: true) var id: String = UUID().uuidString
+    @Persisted var time: Date //時間
+    @Persisted var amount: Int //回数
+    @Persisted var type: StoolType //種類
+}
