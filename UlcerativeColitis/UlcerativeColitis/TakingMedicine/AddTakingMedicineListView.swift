68eb25e90b86a4e2ab748d832341bb71d405f4da
yutaizawa <yutamac2512@icloud.com>
2025-11-24 20:01:14 +0900

    AddTakingMedicineView:追加メソッド追加

--- UlcerativeColitis/UlcerativeColitis/TakingMedicine/AddTakingMedicineListView.swift
+++ UlcerativeColitis/UlcerativeColitis/TakingMedicine/AddTakingMedicineListView.swift
@@ -11,61 +11,88 @@ import RealmSwift
 struct AddTakingMedicineListView: View {
     
     @ObservedResults(MedicineInfo.self) var medicineInfo
+    @ObservedResults(DayRecord.self) var dayRecord
     
     @State private var dosageTextField: [String: String] = [:]
     @State private var isSelected = false
     @State private var selectedMedicine = Set<String>()
     
+    @Binding var selectDay: Date
+    
     var body: some View {
-        List {
-            ForEach(medicineInfo, id: \.id) { medicine in
-                HStack {
-                    Image(systemName:
-                            selectedMedicine.contains(medicine.id)
-                          ? "checkmark.circle"
-                          : "circle"
-                    )
-                    
-                    Text(medicine.medicineName)
-                    
-                    Spacer()
-                    
-                    if let dosage = medicine.dosage {
-                        TextField(
-                            "",
-                            text: Binding(
-                                get: { dosageTextField[medicine.id] ?? "\(medicine.dosage ?? 0)" },
-                                set: { dosageTextField[medicine.id] = $0 }
-                            )
+        VStack {
+            Button(action: {
+                addTakingMedicine()
+            }) {
+                Image(systemName: "plus")
+            }
+            List {
+                ForEach(medicineInfo, id: \.id) { medicine in
+                    HStack {
+                        Image(systemName:
+                                selectedMedicine.contains(medicine.medicineName)
+                              ? "checkmark.circle"
+                              : "circle"
                         )
-                        .keyboardType(.numberPad)
-                        .foregroundColor(.black)
-                        .textFieldStyle(.roundedBorder)
-                        .frame(width: 50)
-                        .multilineTextAlignment(.trailing)
-                        Text(medicine.unit?.customUnitName ?? medicine.unit?.unitName?.japaneseText ?? UnitArrayEnum.tablet.japaneseText)
+                        
+                        Text(medicine.medicineName)
+                        
+                        Spacer()
+                        
+                        if let dosage = medicine.dosage {
+                            TextField(
+                                "",
+                                text: Binding(
+                                    get: { dosageTextField[medicine.id] ?? "\(medicine.dosage ?? 0)" },
+                                    set: { dosageTextField[medicine.id] = $0 }
+                                )
+                            )
+                            .keyboardType(.numberPad)
                             .foregroundColor(.black)
-                    } else {
-                        Text("-")
-                            .foregroundColor(.secondary)
-                        Text(medicine.unit?.customUnitName ?? medicine.unit?.unitName?.japaneseText ?? UnitArrayEnum.tablet.japaneseText)
-                            .foregroundColor(.secondary)
+                            .textFieldStyle(.roundedBorder)
+                            .frame(width: 50)
+                            .multilineTextAlignment(.trailing)
+                            Text(medicine.unit?.customUnitName ?? medicine.unit?.unitName?.japaneseText ?? UnitArrayEnum.tablet.japaneseText)
+                                .foregroundColor(.black)
+                        } else {
+                            Text("-")
+                                .foregroundColor(.secondary)
+                            Text(medicine.unit?.customUnitName ?? medicine.unit?.unitName?.japaneseText ?? UnitArrayEnum.tablet.japaneseText)
+                                .foregroundColor(.secondary)
+                        }
                     }
-                }
-                .onTapGesture {
-                    isSelected.toggle()
-                    if selectedMedicine.contains(medicine.id) {
-                        selectedMedicine.remove(medicine.id)
-                    } else {
-                        selectedMedicine.insert(medicine.id)
+                    .onTapGesture {
+                        isSelected.toggle()
+                        if selectedMedicine.contains(medicine.medicineName) {
+                            selectedMedicine.remove(medicine.medicineName)
+                        } else {
+                            selectedMedicine.insert(medicine.medicineName)
+                        }
+                        print(selectedMedicine)
                     }
-                    print(selectedMedicine)
                 }
             }
+        }
+    }
+    
+    private func addTakingMedicine() {
+        let realm = try! Realm()
+        
+        try! realm.write {
+            let dayModel = DayRecord()
+            dayModel.date = selectDay
+            
+            for medicineName in selectedMedicine {
+                let medicine = TakingMedicine()
+                medicine.medicineName = medicineName
+                dayModel.takingMedicine.append(medicine)
+            }
+            
+            realm.add(dayModel)
         }
     }
 }
 
 #Preview {
-    AddTakingMedicineListView()
+    AddTakingMedicineListView(selectDay: .constant(Date()))
 }
